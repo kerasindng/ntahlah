ff0fdb19c222d92a8e674067dd15d4766359315f
kerasindng <149295661+kerasindng@users.noreply.github.com>
2023-12-13 05:32:06 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+# Pindah ke direktori proyek
+cd /root/Userbothon/
+
+# Instalasi dependensi
 pip install -U -r requirements.txt
+
+# Menjalankan aplikasi Clever
 python3 clever.py &
+
+# Menunggu beberapa saat agar aplikasi dapat memulai
+sleep 5
+
+# Menjalankan modul Indomie
 python3 -m indomie
