35fe923aeb8054ea7705b5c2e571885a5899cad8
kerasindng <149295661+kerasindng@users.noreply.github.com>
2023-12-13 05:22:06 +0700

    Update clever.py

--- clever.py
+++ clever.py
@@ -1,13 +1,11 @@
-import os
+# app.py
 from flask import Flask
-from flask_restful import Resource, Api
 
 app = Flask(__name__)
-api = Api(app)
 
-class Greeting (Resource):
-    def get(self):
-        return "Clever Cloud is Up & Running!"
+@app.route('/')
+def hello():
+    return 'Hello, Clever Cloud!'
 
-api.add_resource(Greeting, '/')
-app.run(host="0.0.0.0", port=os.environ.get("PORT", 8080))
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
